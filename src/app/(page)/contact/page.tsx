ab06b1ead7b3311a241de00f0aef227b92efbdfb
Lecuyer-Quentin <quentin.lecuyer@mailfence.com>
2024-02-07 18:33:31 +0100

    contact page

---
+++ src/app/(page)/contact/page.tsx
@@ -0,0 +1,9 @@
+import Contact from "@/app/ui/contact"
+
+export default function ContactPage() {
+  return (
+    <main className=' flex flex-col justify-between items-center pt-40 min-h-screen'>
+        <Contact />
+    </main>
+  )
+}
