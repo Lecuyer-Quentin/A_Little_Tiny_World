cbb8064d5b73677352fcd26148a28294d564bf1c
Lecuyer-Quentin <quentin.lecuyer@mailfence.com>
2024-02-08 22:09:24 +0100

    carousel sm

---
+++ src/app/components/carousel/index.tsx
@@ -0,0 +1,47 @@
+'use client'
+
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel"
+import Autoplay from "embla-carousel-autoplay"
+import CardSM from "../card/card-sm"
+
+type CarouselSMProps = {
+    data: {
+        _id: number | string
+        title: string
+        description: string
+        image: string
+        alt: string
+    }[]
+}
+
+  export default function CarouselSM({ data }: CarouselSMProps) {
+
+    const renderContent = ({ data }: CarouselSMProps) => {
+        return (
+            data.map((item, _id) => (
+                <CarouselItem key={_id} className="basis-1/2 lg:basis-1/4">
+                    <CardSM data={item} />
+                </CarouselItem>
+            ))
+        )
+    }
+
+    const renderCarousel = () => {
+        return (
+            <Carousel 
+                plugins={[
+                    Autoplay({ delay: 9000, stopOnHover: true, waitForAction: true }),
+                ]}
+                opts={{
+                    loop: true,
+                    }}
+                className="w-full h-auto"
+            >
+                <CarouselContent>
+                    {renderContent({ data })}
+                </CarouselContent>
+            </Carousel>
+        )
+    }
+    return renderCarousel()
+}
