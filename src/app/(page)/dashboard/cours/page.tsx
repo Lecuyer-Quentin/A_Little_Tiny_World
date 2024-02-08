16e027c808de9ba6c77bd0d26dda5b27b05b90a4
Lecuyer-Quentin <quentin.lecuyer@mailfence.com>
2024-02-08 22:05:20 +0100

    add dashboard/ pages

---
+++ src/app/(page)/dashboard/cours/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function CoursPage() {
+  return (
+    <div>CoursPage</div>
+  )
+}
---
+++ src/app/(page)/dashboard/promotions/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function PromotionsPage() {
+  return (
+    <div>PromotionsPage</div>
+  )
+}
