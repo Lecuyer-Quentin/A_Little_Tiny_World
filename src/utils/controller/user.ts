9eacf1cc97e024ad4b0c15c304f4aab68a83ac15
Lecuyer-Quentin <quentin.lecuyer@mailfence.com>
2024-02-08 22:01:04 +0100

    correction  getUserByEmail

--- src/utils/controller/user.ts
+++ src/utils/controller/user.ts
@@ -8,14 +8,12 @@ import { NextResponse } from "next/server";
 // Omit is a utility type that allows you to create a new type by omitting properties from an existing type.
 type Props = Omit<UserModel, 'createdAt' | 'updatedAt'>;
 
-////////////////////////////////////////////////////////////////////////////////////////
 //  This function is used to create a new user in the database
 export const createUser = async (user: Props) => {
 
     const name = user.email.split('@')[0].split('.')[0].split('_').join(' ');
     const firstChar = name.charAt(0).toUpperCase();
     const nameToRender = firstChar + name.slice(1);
-
 
     // User object that will be added to the database
     const userAdded : Props = {
@@ -44,22 +42,32 @@ export const createUser = async (user: Props) => {
         return NextResponse.json({ message: "Error creating User"}, { status: 500 });
     }
 };
-////////////////////////////////////////////////////////////////////////////////////////
 
 
 // This function is used to find a user by email
 export const getUserByEmail = async (email: string) => {
     if (!email) throw new Error('Email is required');
-    const user = await User.findOne({ email});
-    if (!user) throw new Error('No User Found')
-    return user;
+
+    try {
+        await dbConnect();
+        const user = await User.findOne({email});
+        return user;
+    } catch (error) {
+        throw new Error('Error finding user');
+    }
+
 }
 
+// This function is used to authorize a user
 export const authorizeUser = async (credentials: { email: string, password: string }) => {
     const { email, password } = credentials;
-    const user = await getUserByEmail(email);
-    if (!user) throw new Error('No user found');
-    const isValid = await comparePassword(password, user.password);
-    if (!isValid) throw new Error('Invalid password');
-    return user;
+    try{
+        const user = await getUserByEmail(email);
+        if (!user) throw new Error('No user found');
+        const isValid = await comparePassword(password, user.password);
+        if (!isValid) throw new Error('Invalid password');
+        return user;
+    } catch (error) {
+        throw new Error('Error authorizing user');
+    }
 }
