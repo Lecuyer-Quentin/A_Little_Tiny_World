8274ba9ee03c32c5e4e9b64f2d263c145338ca8b
Lecuyer-Quentin <quentin.lecuyer@mailfence.com>
2024-02-10 17:51:31 +0100

    ui hero

--- src/app/ui/home/hero/index.tsx
+++ src/app/ui/home/hero/index.tsx
@@ -1,21 +1,36 @@
 import Image from 'next/image'
 
-type Props = {
-  image: string,
-  title: string,
-  description: string
-}
 
-export default function Hero({image, title, description}: Props) {
+export default function Hero() {
+
+  const renderBackground = () => {
+    const images = '/images/champs-de-lavande.jpg'
+    return (
+      <div className='absolute h-full w-full top-0 left-0 -z-10 opacity-70 rounded-lg'
+        style={{
+          backgroundImage: `url(${images})`, 
+          backgroundPosition: 'center', 
+          backgroundSize: 'cover',
+        }} />      
+    )
+  }
+
+  const renderLogo = () => {
+    const image = '/images/logo-hero.png'
+    return (
+      <Image src={image} alt="logo" width={600} height={600} className='z-10' />
+    )
+  }
+
+  const renderHero = () => {
+    return (
+      <section className='w-full h-[10rem] flex flex-col justify-center items-center relative rounded-lg'>
+        {renderBackground()}
+        <div className='absolute w-full h-full bg-black opacity-10 rounded-lg' />
+        {renderLogo()}
+      </section>
+    )
+  }
   
-  return (
-    <section className='w-full h-auto flex flex-col justify-center items-center'>
-    <Image src={image} alt="logo" width={400} height={400} />
-
-    <div className=''>
-      <h1 className=''>{title}</h1>
-      <p className=''>{description}</p>
-    </div>
-    </section>
-  )
+  return renderHero()
 }
