64e95bbd94a376e1a765d30942f56b983bdc6fa2
Lecuyer-Quentin <quentin.lecuyer@mailfence.com>
2024-05-27 22:47:07 +0200

    search result component

---
+++ components/search/searchResult.php
@@ -0,0 +1,7 @@
+<div class="offcanvas offcanvas-bottom" tabindex="-1" id="search_result_offcanvas" aria-labelledby="search_result_offcanvas">
+    <div class="offcanvas-header">
+        <h5 class="offcanvas-title">Résultats de recherche</h5>
+        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body d-flex justify-content-around flex-wrap" id="search-result"></div>
+</div>
