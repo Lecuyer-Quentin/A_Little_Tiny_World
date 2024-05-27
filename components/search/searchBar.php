48dda3897f1b12dd6c13a6fcecae21b7b910fedc
Lecuyer-Quentin <quentin.lecuyer@mailfence.com>
2024-05-27 22:46:39 +0200

    search bar component

---
+++ components/search/searchBar.php
@@ -0,0 +1,8 @@
+    <form id="search-form" action="controllers/product/search.php" method="POST">
+        <div class="input-group">
+            <input type="text" class="form-control" name="search" placeholder="À l'assaut de la perle rare" id="search-input">
+            <button type="submit" class="btn btn-primary" data-bs-toggle="offcanvas" data-bs-target="#search_result_offcanvas" aria-controls="offcanvasExample">
+                <img src="assets/svg/search_heart.svg" width="40" height="40" alt="Rechercher">
+            </button>
+        </div>
+    </form>
