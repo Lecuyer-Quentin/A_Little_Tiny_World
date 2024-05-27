16175a8aa6e47aafddcf02c9afca1ea97953f704
Lecuyer-Quentin <quentin.lecuyer@mailfence.com>
2024-05-27 22:37:02 +0200

    alert component

---
+++ components/alert/index.php
@@ -0,0 +1,17 @@
+
+<div class="toast-container position-fixed top-0 end-0 p-3">
+
+    <div class="toast" id="toast-success" role="alert" aria-live="assertive" aria-atomic="true">
+        <div class="toast-header bg-success text-white">
+            <strong class="mr-auto">L'opération a réussi</strong>
+        </div>
+        <div class="toast-body" id="toast-body-success"></div>
+    </div>
+
+    <div class="toast" id="toast-error" role="alert" aria-live="assertive" aria-atomic="true">
+        <div class="toast-header bg-danger text-white">
+            <strong class="mr-auto">Une erreur est survenue</strong>
+        </div>
+        <div class="toast-body" id="toast-body-error"></div>
+    </div>
+</div>
