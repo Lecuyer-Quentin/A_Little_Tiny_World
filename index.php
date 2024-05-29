dc58ebdfa86b338fd36b175e713ac911c37f7307
Lecuyer-Quentin <quentin.lecuyer@mailfence.com>
2024-05-29 22:39:19 +0200

    stylesheet bootstrap.min.css

--- index.php
+++ index.php
@@ -24,8 +24,8 @@ $metaLogo = get_JSON('config.json', 'image', 'metaLogo');
             <link rel = "icon" href = "<?php echo $item['src']; ?>">
         <?php endif; ?>
     <?php endforeach; ?>
-    
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <link rel="stylesheet" type="text/css" href = "node_modules/bootstrap/dist/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href = "assets/custom.css">
     <link rel="manifest" href="manifest.webmanifest">
 
