c35d24754a7ce241d8e42fd7110a04720357aece
Lecuyer-Quentin <quentin.lecuyer@mailfence.com>
2024-05-29 21:51:11 +0200

    activate user controller

---
+++ controllers/auth/activate.php
@@ -0,0 +1,40 @@
+<?php
+require_once '../../config/db.php';
+require_once '../../page.inc.php';
+global $pdo;
+
+if(isset($_GET['token']) && !empty($_GET['token'])){
+    $token = $_GET['token'];
+    $user = new UtilisateurRepo($pdo);
+    $user = $user->activate($token);
+    if($user){
+        $response = array(
+            //'status' => 'success',
+            //'message' => 'Votre compte a bien été activé, vous pouvez vous connecter',
+            'redirect' => '../../index.php?page=home',
+        );
+    }else{
+        $response = array(
+            //'status' => 'error',
+            //'message' => 'Une erreur est survenue lors de l\'activation de votre compte',
+            'redirect' => '../../index.php?page=home',
+        );
+    }
+}else{
+    $response = array(
+        //'status' => 'error',
+        //'message' => 'Token invalide',
+        'redirect' => '../../index.php?page=home',
+    );
+}
+
+header('Location: '.$response['redirect']);
+
+//echo json_encode($response);
+
+
+
+
+
+
+
