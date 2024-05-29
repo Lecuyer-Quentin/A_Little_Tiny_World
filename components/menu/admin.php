937007438af3fe6780dfd5b9fc6f977942223921
Lecuyer-Quentin <quentin.lecuyer@mailfence.com>
2024-05-29 21:48:10 +0200

    admin menu

--- components/menu/admin.php
+++ components/menu/admin.php
@@ -1,7 +1,6 @@
 <?php
     $data = get_JSON('config.json', 'menu', 'admin');
     $items = $data['items'];
-    $url_back = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : 'index.php';
 ?>
 
 <nav class="navbar navbar-expand-md navbar-light bg-light">
@@ -9,13 +8,6 @@
     <button class="navbar-toggler mt-1 border-0 shadow-none focus:outline-none focus:ring-0" type="button" data-bs-toggle="collapse" data-bs-target="#admin_menu" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
-
-    <a href="<?php echo $url_back; ?>">
-        <button class="btn btn-dark focus:outline-none focus:ring-0">
-            <img src="assets/svg/arrow_square_right.svg" alt="Retour">
-        </button>
-
-    </a>
 
     <div class="collapse navbar-collapse" id="admin_menu">
         <ul class="navbar-nav flex-row justify-content-center flex-wrap">
